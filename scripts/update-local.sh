e4e2e6de52453f023e685e123d8c79eeff62bf77
Albasrawi <alikamel.com@gmail.com>
2019-12-17 14:14:14 +0300

    Update

--- scripts/update-local.sh
+++ scripts/update-local.sh
@@ -17,34 +17,10 @@
 # dpkg-deb --bZlzma Projects/<projectname> <output folder>
 # dpkg-deb -bZlzma ~/Projects/Theos/tweaks/motuumls/.theos/_ debs
 # dpkg-deb -bZlzma ~/Projects/Theos/tweaks/fastdel/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/flatfields/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/intelligentpower/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/nodropsheet/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/predictionstickaround/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/sera/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/libCSWeather/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/foresight/.theos/_ debs
+# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/flightradar24/.theos/_ debs
 # dpkg-deb -bZlzma ~/Projects/Theos/tweaks/switcherRadii/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/alwaysPlaying/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/selectivecontrols/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/cscolorpicker/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/libcspreferences/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/pulltoclear/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/Consolidation/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/reachalert/.theos/_ debs
 # dpkg-deb -bZlzma ~/Projects/Theos/tweaks/copyvideourl/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/attibutionless/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/ToothModule/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/switcherRadiiXI/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/stretcher/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/shuffle/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/ebullience/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/simmerdown/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/crux/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/covert/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/bubbles/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/cyground/.theos/_ debs
-# dpkg-deb -bZlzma ~/Projects/Theos/tweaks/wtfeclipse/.theos/_ debs
+
 
 
 #packages
